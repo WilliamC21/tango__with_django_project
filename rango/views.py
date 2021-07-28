9dfb97bff727e4c0f19954c5ae28cdd1db86f63a
WilliamC21 <84275546+WilliamC21@users.noreply.github.com>
2021-07-28 21:58:58 +0900

    enabling category pages

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,9 @@
 from typing import OrderedDict
+from django.core.exceptions import NON_FIELD_ERRORS
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     #dictionary used to pass into template as context()
@@ -17,3 +19,27 @@ def index(request):
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by William'}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    #create a context dictionary(will be passed to html file)
+    context_dict = {}
+
+    try:
+        #try to find category name slug with given name(prameter passed)
+        category = Category.objects.get(slug=category_name_slug)
+
+        #retrieve all pages associated
+        pages = Page.objects.filter(category = category)
+        
+        #add these pages to the dictionary
+        context_dict['pages'] = pages
+
+        #this used to verify category exists
+        context_dict['category'] = category
+    except Category.DoesNotExist: #throw exception if no such category
+        #avoid doing anything, disaply no category message
+        context_dict['category'] = None
+        context_dict['pages'] = None\
+    
+    return render(request, 'rango/category.html', context=context_dict)
+
