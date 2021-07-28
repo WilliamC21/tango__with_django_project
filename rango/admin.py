7616a0db2e0d155f0f9c813d4ed87c35f3c9da8d
WilliamC21 <84275546+WilliamC21@users.noreply.github.com>
2021-07-28 18:21:46 +0900

    edit to admin page

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 from rango.models import Category, Page
 
+class PageAdmin (admin.ModelAdmin):
+    list_display = ('title','category', 'url')
+
 admin.site.register(Category)
-admin.site.register(Page)
-# Register your models here.
+admin.site.register(Page, PageAdmin)
+
+
