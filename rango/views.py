00cb602b5b2e76d722f03fd8afc2d7d4cb3a62fc
WilliamC21 <84275546+WilliamC21@users.noreply.github.com>
2021-07-28 20:33:06 +0900

    cat list

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,15 @@
+from typing import OrderedDict
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
     #dictionary used to pass into template as context()
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'} #boldmessage: mathces HTML doc name
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
    
     #returns rendered response, first parameter = desired template
     return render(request, 'rango/index.html', context=context_dict)
